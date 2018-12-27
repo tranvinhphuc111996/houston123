deb6b911398099811db778458bd5ecf155298287
John Dylan <johndylan1995@gmail.com>
2018-12-27 21:06:28 +0700

    Update app remote with nodejs

--- firmwarechecker.js
+++ firmwarechecker.js
@@ -5,36 +5,75 @@ let fs = require('fs');
 async function check() {
 	try {
 		let fixLogitech = 'dwc_otq.fiq_enable=1 dwc_otg.fiq_fsm_enable=1 dwc_otg.fiq_fsm_mask=0x03';
+		let autoboot = 'sudo node /home/pi/Desktop/houston123/main.js'
+		
+		let pid;
+		try {			
+			await exec('pidof python').then((data) => {
+				pid = data.stdout;
+			});
+		} catch (error) {
+			
+		}
+		
+		if (pid != null) {
+			console.log('Python is running...');		
+			pid = pid.replace('\n', '');
+			pid = pid.toString().split(' ');
+			for (let p of pid) {
+				try {
+					await exec(`kill -9 ${p}`);
+					console.log(`${p} was killed!`);
+				} catch (error) {
+					console.log(`Cant kill pid:${p}`);
+				}
+			}
+		}
 
-		await exec('find /home/pi/Desktop/TimeKeeper/houston123').then(() => {
+		await exec('find /home/pi/Desktop/houston123').then(() => {
 			console.log('Source is ready');
         });
-		await exec('git pull', {cwd: '/home/pi/Desktop/TimeKeeper/houston123'}).then(() => {
+		await exec('git pull', {cwd: '/home/pi/Desktop/houston123'}).then(() => {
 			console.log('Pull lastest version');
 		});
 		await fs.readFile('/boot/cmdline.txt', 'utf8', (err, data) => {
+			data = data.replace('\r', '');
+			data = data.replace('\n', '');			
 			if (typeof data == 'string' && data.indexOf(fixLogitech) == -1) {
 				data += ' ' + fixLogitech;
 				exec(`echo "${data}" > 'cmdline.txt'`, {cwd: '/boot'}).then(() => {
 					console.log('Fixed webcam logitech');
 				});
-			} else { 
+			} else {
 				console.log('Webcam logitech was fixed');
+			}
+		});
+		await fs.readFile('/etc/rc.local', 'utf8', (err, data) => {
+			data = data.replace('\r', '');
+			data = data.replace('\n', '');						
+			if (typeof data == 'string' && data.indexOf(autoboot) == -1) {
+				data += autoboot;
+				exec(`echo "${data}" > 'rc.local'`, {cwd: '/etc'}).then(() => {
+					console.log('Auto boot is adding');
+				});
+			} else {
+				console.log('Autoboot is ready');
 			}
 		});
         await exec('apt-get install fswebcam').then(() => {
 			console.log('Install lib fswebcam');
-        });
-        await exec('python setup.py install', {cwd: '/home/pi/Desktop/TimeKeeper/houston123/SPI-Py'}).then(() => {
-			console.log('Install lib SPI');
-        });
+		});		
         await exec('pip install websocket-client').then(() => {
 			console.log('Install lib websocket-client');
 		});
-		exec('python ThreadRealWork_25_12_2018.py', {cwd: '/home/pi/Desktop/TimeKeeper/houston123'});
+        await exec('python setup.py install', {cwd: '/home/pi/Desktop/houston123/SPI-Py'}).then(() => {
+			console.log('Install lib SPI');
+		});
+		
+		exec('python ThreadRealWork_25_12_2018.py', {cwd: '/home/pi/Desktop/houston123'});
 	} catch (err) {
 		switch (err.cmd) {
-			case 'find /home/pi/Desktop/TimeKeeper/houston123': {
+			case 'find /home/pi/Desktop/houston123': {
 				gitClone();
 			} break;
 			default:
@@ -46,7 +85,7 @@ async function check() {
 async function gitClone() {
 	try {
 		console.log('Clone project TimeKeeper');		
-		await exec('cd /home/pi/Desktop/TimeKeeper');
+		await exec('cd /home/pi/Desktop');
 		await exec('git clone https://github.com/tranvinhphuc111996/houston123.git');
 		init();
 	} catch (err) {
@@ -54,4 +93,9 @@ async function gitClone() {
 	}  
 }
 
-exports.checkConnect = check;
+function reboot() {
+	exec('reboot');
+}
+
+exports.update = check;
+exports.reboot = reboot;
--- main.js
+++ main.js
@@ -1,17 +1,33 @@
-let socket = require('socket.io-client')('https://houston123.ddns.net');
+let socket = require('socket.io-client')('https://houston123.ddns.net/devices/timekeeper');
 let firmwarechecker = require('./firmwarechecker');
+let os = require('os');
+
+//firmwarechecker.update()
+
+function imStillALive() {
+	socket.emit('status', {status: 'keepalive', uptime: os.uptime()})
+}
+  
+setInterval(imStillALive, 2 * 60 * 1000);
 
 socket.on('connect', () => {
 	console.log('Connected to server houston123.ddns.net');
-	socket.emit('kiem-tra-ket-noi');
+	socket.emit('getDevicesToken');
 });
-socket.on('ket-noi-thanh-cong', (data) => {
-    socket.emit('gan-ten-cho-socket', {username: 'DIATK001', password: ''});
+
+socket.on('getDevicesToken', (data) => {
+	console.log(data);
+})
+
+socket.on('update', (data) => {
+	firmwarechecker.update()
 });
-socket.on('gan-ten-cho-socket-thanh-cong', () => {
-	console.log('Gan ten thanh cong');
-	firmwarechecker.checkConnect();
+
+socket.on('reboot', (data) => {
+	socket.emit('status', {status: 'reboot'})
+	firmwarechecker.reboot();
 });
+
 socket.on('disconnect', () => {
     console.log('Loss connected to server houston123.ddns.net');
 });
